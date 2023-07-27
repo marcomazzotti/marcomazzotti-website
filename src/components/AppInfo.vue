b620e039a50723d56d7f9ea2396725d628a515da
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-27 17:53:43 +0200

    Edit: appInfo

--- src/components/AppInfo.vue
+++ src/components/AppInfo.vue
@@ -7,11 +7,15 @@ export default {
 <template>
   <div class="container py-5 d-flex align-items-center">
     <div class="left me-5">
-      <p>Hi there, my name is Marco and I live in Milan, Italy. <br>
+      <div class="img-container pb-5">
+        <img src="../assets/Foto Social.jpeg" alt="">
+      </div>
+      <p class="px-4">Hi there, my name is Marco and I live in Milan, Italy. <br>
         I studied graphic design, but over the last few years, I developed an interest in the world of web development.
         <br>
-        I've learned various front-end and back-end languages and technologies, and I enjoy using them to create beautiful
-        and functional websites.</p>
+        I've learned various front-end and back-end languages and technologies, and I enjoy using them to create modern
+        and functional websites.
+      </p>
     </div>
     <div class="right ms-5">
       <h2><strong>Jr. Full Stack<br>Web Developer</strong></h2>
@@ -23,16 +27,43 @@ export default {
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
 
+.container {
+  border-bottom: 1px solid red;
+}
+
 .left {
   width: 50%;
   background-color: black;
   color: white;
-  padding: 3rem 2rem;
+  padding: 4rem 3rem;
   border-radius: 10px;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+
+  p {
+    font-size: 1.1rem;
+  }
+
+  .img-container {
+    text-align: end;
+  }
+
+  img {
+    max-width: 40%;
+    border-radius: 50%;
+    position: relative;
+
+  }
 }
 
 .right {
   width: 50%;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+
+  h2 {
+    font-size: 3rem;
+  }
 
   .btn {
     border: 1px solid black !important;
